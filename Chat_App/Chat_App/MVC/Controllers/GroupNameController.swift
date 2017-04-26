fd934b5ace7384c64424348f6d948eeb44168847
Deepsikha <ldipshikhal1986@gmail.com>
2017-04-26 11:43:32 +0530

    change header

---
+++ Chat_App/Chat_App/MVC/Controllers/GroupNameController.swift
@@ -0,0 +1,35 @@
+//
+//  GroupNameController.swift
+//  Chat_App
+//
+//  Created by devloper65 on 4/26/17.
+//  Copyright © 2017 LaNet. All rights reserved.
+//
+
+import UIKit
+
+class GroupNameController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
