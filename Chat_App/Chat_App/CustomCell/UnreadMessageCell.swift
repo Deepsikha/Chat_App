680ada822541cc2a4f72ee334a94faf2b2747c6d
gaddamsrikanth <gaddam.srikanth.gollapally@gmail.com>
2017-05-08 13:44:36 +0530

    image upload/download

---
+++ Chat_App/Chat_App/CustomCell/UnreadMessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  UnreadMessageCell.swift
+//  Chat_App
+//
+//  Created by Developer88 on 5/6/17.
+//  Copyright © 2017 LaNet. All rights reserved.
+//
+
+import UIKit
+
+class UnreadMessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
