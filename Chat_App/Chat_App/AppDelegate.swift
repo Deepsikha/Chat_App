953c3915fc8d5926692b898ce75ed236880c2aad
gaddamsrikanth <gaddam.srikanth.gollapally@gmail.com>
2017-05-16 12:40:18 +0530

    facebook integration

--- Chat_App/Chat_App/AppDelegate.swift
+++ Chat_App/Chat_App/AppDelegate.swift
@@ -12,6 +12,7 @@ import Fabric
 import Crashlytics
 import PhotosUI
 import WebRTC
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -36,6 +37,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Fabric.with([Crashlytics.self()])
         self.logUser()
         
+        //Facebook 
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+
         if(UserDefaults.standard.value(forKey: "id") == nil) {
             
             let rootVC = SetProfileController()
@@ -71,8 +75,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        FBSDKAppEvents.activateApp()
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -91,6 +94,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+    
     func logUser() {
         
         Crashlytics.sharedInstance().setUserEmail("lanetteam.milans@gmail.com")
